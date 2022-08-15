4efed5bcf315190f4c1c6e8fbc5c7f1f0be64f97
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-15 13:20:46 -0400

    Added GH Link

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>HEY</h1>
         <Weather city="Paris" />
+        <a
+          href="https://github.com/courtneyyatteau/Weather-App-React"
+          target="_blank"
+          rel="noreferrer"
+          class="App-link"
+        >
+          Github Link
+        </a>
       </header>
     </div>
   );
