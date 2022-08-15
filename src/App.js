ef04e50b55e0558a072cfe6ffda51f498e2949c5
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-08-15 14:32:49 -0400

    Folder Cleanup & Favicon

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-        <a
-          href="https://github.com/courtneyyatteau/Weather-App-React"
-          target="_blank"
-          rel="noreferrer"
-          class="App-link"
-        >
-          Github Link
-        </a>
-      </header>
+      <Weather />
+      <a
+        href="https://github.com/courtneyyatteau/Weather-App-React"
+        target="_blank"
+        rel="noreferrer"
+        class="App-link"
+      >
+        Github Link
+      </a>
     </div>
   );
 }
